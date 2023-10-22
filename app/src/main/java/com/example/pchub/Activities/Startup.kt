aa1b9acf9b460348b2e42ce26e071606335c8265
Adnan Tinte Chohan <adti.chohan.up@phinmaed.com>
2023-10-23 01:15:02 +0800

    Added a startup screen along with its backend and a ViewPageAdapter. Should work.

--- app/src/main/java/com/example/pchub/Activities/Startup.kt
+++ app/src/main/java/com/example/pchub/Activities/Startup.kt
@@ -1,23 +1,103 @@
-package com.example.pchub.Activities
+package com.example.onboarding
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.example.pchub.Activities.ViewPageAdapter
 import com.example.pchub.R
+import com.example.pchub.viewmodel.mainscreen
 
 class Startup : AppCompatActivity() {
+
+    private lateinit var mSlideViewPager: ViewPager
+    private lateinit var mDotLayout: LinearLayout
+    private lateinit var backBtn: Button
+    private lateinit var nextBtn: Button
+    private lateinit var skipBtn: Button
+    private lateinit var dots: Array<TextView>
+    private lateinit var viewPagerAdapter: ViewPageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startup)
-        val EnterButton: Button = findViewById(R.id.et_button)
 
-        EnterButton.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+        backBtn = findViewById(R.id.backBtn)
+        nextBtn = findViewById(R.id.nextBtn)
+        skipBtn = findViewById(R.id.skipButton)
+        mSlideViewPager = findViewById(R.id.slideViewPager)
 
+
+        backBtn.setOnClickListener {
+            if (getItem(0) > 0) {
+                mSlideViewPager.setCurrentItem(getItem(-1), true)
+            }
         }
 
+        nextBtn.setOnClickListener {
+            if (getItem(0) < 3) {
+                mSlideViewPager.setCurrentItem(getItem(1), true)
+            } else {
+                val intent = Intent(this, mainscreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        skipBtn.setOnClickListener {
+            val intent = Intent(this, mainscreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        viewPagerAdapter = ViewPageAdapter(this)
+        mSlideViewPager.adapter = viewPagerAdapter
+
+        setUpIndicator(0)
+        mSlideViewPager.addOnPageChangeListener(viewListener)
+    }
+
+    private fun setUpIndicator(position: Int) {
+        dots = Array(4) { TextView(this) }
+        mDotLayout.removeAllViews()
+
+        for (i in dots.indices) {
+            dots[i] = TextView(this)
+            dots[i].text = Html.fromHtml("&#8226")
+            dots[i].textSize = 35f
+            dots[i].setTextColor(resources.getColor(R.color.inactive, theme))
+            mDotLayout.addView(dots[i])
+        }
+
+        dots[position].setTextColor(resources.getColor(R.color.active, theme))
+    }
+
+    private val viewListener = object : ViewPager.OnPageChangeListener {
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            // Do nothing here
+        }
+
+        override fun onPageSelected(position: Int) {
+            setUpIndicator(position)
+
+            if (position > 0) {
+                backBtn.visibility = View.VISIBLE
+            } else {
+                backBtn.visibility = View.INVISIBLE
+            }
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {
+            // Do nothing here
+        }
+    }
+
+    private fun getItem(i: Int): Int {
+        return mSlideViewPager.currentItem + i
     }
 }
---
+++ app/src/main/java/com/example/pchub/Activities/ViewPageAdapterr.kt
@@ -0,0 +1,64 @@
+package com.example.pchub.Activities
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import com.example.pchub.R
+
+class ViewPageAdapter(private val context: Context) : PagerAdapter() {
+
+    private val images = arrayOf(
+        R.drawable.image1,
+        R.drawable.image2,
+        R.drawable.image3,
+        R.drawable.image4
+    )
+
+    private val headings = arrayOf(
+        R.string.heading_one,
+        R.string.heading_two,
+        R.string.heading_three,
+        R.string.heading_fourth
+    )
+
+    private val descriptions = arrayOf(
+        R.string.desc_one,
+        R.string.desc_two,
+        R.string.desc_three,
+        R.string.desc_fourth
+    )
+
+    override fun getCount(): Int {
+        return headings.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view == `object` as LinearLayout
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(R.layout.slider_layout, container, false)
+
+        val slideTitleImage = view.findViewById<ImageView>(R.id.titleImage)
+        val slideHeading = view.findViewById<TextView>(R.id.textTitle)
+        val slideDescription = view.findViewById<TextView>(R.id.textDescription)
+
+        slideTitleImage.setImageResource(images[position])
+        slideHeading.setText(headings[position])
+        slideDescription.setText(descriptions[position])
+
+        container.addView(view)
+
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as LinearLayout)
+    }
+}
